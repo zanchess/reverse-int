734b8013bb43c18cca11df52df584efef72a7431
Alexander <46690347+zanchess@users.noreply.github.com>
2020-02-20 00:30:01 +0300

    feat: end revers int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+  	let str = "";
+	let a = String(n);
+	for (let i = 0; i < a.length; i++) {
+		if ((a[0] !== "0") && !isNaN(a[i])) {
+			let num = Math.abs(Number(a[i]));
+			str += String(num);
+		}
+	}
+	let newStr = str.split('').reverse().join('').replace(/^0+/, '');
+	return +newStr;
 }
